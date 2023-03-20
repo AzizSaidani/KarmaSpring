e5773fd44ec042a8cc4278062fe0e137c0b65f71
AzizSaidani <70522017+AzizSaidani@users.noreply.github.com>
2023-03-20 10:53:48 +0100

    first commit

---
+++ src/main/java/com/Karma/KarmaSpring/KarmaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Karma.KarmaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Karma/KarmaSpring/KarmaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Karma.KarmaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
